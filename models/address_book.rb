36ee0d6356da1d3b0aa49290c3adf68b0bf2f827
Thomas Conkwright <tmconkwright@gmail.com>
2017-10-23 19:14:45 -0400

    Assignment 4: remove_entry works

--- models/address_book.rb
+++ models/address_book.rb
@@ -7,14 +7,20 @@ require_relative 'entry'
             @entries = []
         end
 
-    def add_entry(name, phone_number, email)
-        index = 0
-        entries.each do |entry|
-        if name < entry.name
-            break
+        def add_entry(name, phone_number, email)
+            index = 0
+            entries.each do |entry|
+            if name < entry.name
+                break
+            end
+            index+= 1
         end
-        index+= 1
+
+        def remove_entry(name, phone_number, email)
+            entries.each_index do |entry, index|
+                entries.slice!(index) if name == entry.name
+            end
+        end
+        entries.insert(index, Entry.new(name, phone_number, email))
     end
-    entries.insert(index, Entry.new(name, phone_number, email))
-  end
 end
